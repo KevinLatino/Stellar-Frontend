cbf3c22dee5d2e9d6832134ebd7a43e516be36d4
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-06-08 22:33:09 -0600

    sidebar first part finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
+import { Home, BriefcaseBusiness, CalendarHeart, Trophy, Telescope, UserRound } from 'lucide-react';
+
 import './index.css';
 
+
+
 function App() {
+
+  const menu = [
+    {
+      title: "Inicio",
+      icon: <Home />
+    },
+    {
+      title: "Tareas",
+      icon: <BriefcaseBusiness />
+    },
+    {
+      title: "Calendario",
+      icon: <CalendarHeart />
+    },
+    {
+      title: "Ranking",
+      icon: <Trophy />
+    },
+    {
+      title: "Descubre",
+      icon: <Telescope />
+    },
+    {
+      title: "Perfil",
+      icon: <UserRound />
+    }
+  ]
+
   return (
     <>
-    <h1 className='flex justify-center items-center text-2xl'>Bienvenido</h1>
+      <div className="flex">
+        <div className='bg-dark-purple p-5 h-screen pt-5 w-1/6'>
+          <h1 className='text-white text-2xl font-semibold pb-14'>🌠 Stelar</h1>
+          {menu.map((menu, index) => (
+            <>
+            <div key={index} className='flex list-none text-gray-300 text-sm items-center gap-x-2 cursor-pointer p-5'>
+              <span>{menu.icon}</span>
+              <span>{menu.title}</span>
+            </div>
+            </>
+          ))}
+        </div>
+
+
+        <div className='p-7'><h1 className='text-2xl font-semibold'>HomePage</h1></div>
+      </div>
     </>
   )
 }
