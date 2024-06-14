d762ce7e503e67dc9f71b07a113c027c1ffa73d1
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-06-14 16:26:42 -0600

    general changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,12 @@
 import React, { useState } from 'react';
-import {
-  Home,
-  CalendarHeart,
-  Trophy,
-  Telescope,
-  UserRound,
-  FolderHeart
-} from 'lucide-react';
-
+import { NavBar } from './Components/NavBarComponents';
 import './index.css';
 
 function App() {
 
-  const menu = [
-    {},
-    {
-      title: "Inicio",
-      icon: <Home size={22} />
-    },
-    {
-      title: "Tareas",
-      icon: <FolderHeart size={22} />
-    },
-    {
-      title: "Calendario",
-      icon: <CalendarHeart size={22} />
-    },
-    {
-      title: "Ranking",
-      icon: <Trophy size={22} />
-    },
-    {
-      title: "Descubre",
-      icon: <Telescope size={22} />
-    },
-    {
-      title: "Perfil",
-      icon: <UserRound size={22} />
-    },
-    {}
-  ];
-
-
-  const [selectedMenu, setSelectedMenu] = useState(1);
-
-  const handleMenuClick = (index) => {
-    setSelectedMenu(index);
-  };
-
   return (
     <>
-      <div className="flex">
-        <div className='bg-light-white'>
-          <div className='bg-dark-purple pl-8 pt-6 h-screen w-[15rem]'>
-            <h1 className='text-white text-2xl font-semibold pb-14'>🌠 Stellar</h1>
-            {menu.map((menuItem, index) => (
-
-              <div className={`${selectedMenu == index ? 'bg-dark-purple' : 'bg-light-white'}`}>
-                <div
-                  key={index}
-                  onClick={(index > 0) && (index < menu.length - 1) ? () => handleMenuClick(index) : null}
-                  className={`flex bg-dark-purple list-none items-center gap-x-8 cursor-pointer font-semibold p-3 	
-                 ${selectedMenu === index ? 'bg-light-white text-dark-purple rounded-l-full' : 'text-icon-color'}
-                 ${selectedMenu === index + 1 ? 'rounded-br-[2rem] ' : ''} 
-                ${selectedMenu === index - 1 ? 'rounded-tr-[2rem]' : ''}  
-                 `}
-                >
-                  <span className={selectedMenu === index ? 'text-dark-purple' : 'text-icon-color'}>
-                    {menuItem.icon}
-                  </span>
-                  <span>{menuItem.title}</span>
-                </div>
-
-              </div>
-            ))}
-          </div>
-        </div>
-
-          <div className='bg-dark-purple w-full'>
-            <div className='p-7 rounded-tl-[2.7rem] rounded-bl-[2.7rem] bg-light-white w-full h-screen'>
-              <h1 className='text-2xl font-semibold'>Bienvenido</h1>
-            </div>
-          </div>
-        </div>
+      <NavBar />
     </>
   );
 }
