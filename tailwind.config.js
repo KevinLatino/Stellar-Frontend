78234942fd1fc5a986d7c9d84eacd6ec2d9b6b1b
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-06-25 13:54:12 -0600

    General styles changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ export default {
         "dark-black": "#171821",
         "light-black": "#21222D",
         "icon-color": "#BFBFBF",
-        "light-gree": "#A9DFD8"
+        "light-gree": "#A9DFD8",
+
+
+        //More options colors 
+        "Blue-Gray": "#2b2d42",
+        "light-White": "#F5F5F5"
       }
     },
   },
