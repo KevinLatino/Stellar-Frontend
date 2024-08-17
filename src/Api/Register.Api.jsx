7fd7b2fc43d9b5b498440f56ded1c7cc4b7d6cbe
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-08-17 14:13:19 -0600

    adding a form for login

--- src/Api/Register.Api.jsx
+++ src/Api/Register.Api.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const RegisterApi = async (task) => {
+const RegisterApi = async (register) => {
     const base = "http://localhost:3000"
 
-    const res = await axios.post(`${base}/users/create`, task);
+    const res = await axios.post(`${base}/users/create`, register);
 
     return res.data;
 }
--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -42,40 +42,50 @@ const LoginScreen = () => {
                     <p className="text-xl font-medium text-light-White">
                         Crea tu cuenta para comenzar
                     </p>
-                    <div className="relative mb-4 w-full max-w-md">
-                        <UserRound size={20} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                        <input
-                            type="text"
-                            placeholder="Ingresa tu correo electrónico"
-                            className="bg-gray-100 text-gray-800 py-2 pl-12 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            onChange={event => createFormSetter("email")(event.target.value)}
-                            value={formState.email}
-                        />
-                    </div>
-                    <div className="relative mb-4 w-full max-w-md">
-                        <KeyRound size={20} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                        <input
-                            type="password"
-                            placeholder="Ingresa tu contraseña"
-                            className="bg-gray-100 text-gray-800 py-2 pl-12 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            onChange={event => createFormSetter("password")(event.target.value)}
-                            value={formState.password}
-                        />
-                    </div>
-                    {error && <p className="text-red-500 mt-1">{error}</p>}
-                    <button
-                        className="bg-light-blue text-white py-2 px-20 rounded-lg hover:bg-blue-600 transition duration-300 relative"
-                        onClick={handleLogin}
-                        disabled={loginMutation.isLoading}
-                    >
-                        {!loginMutation.isLoading ? 'Login' :
-                            loginMutation.isLoading && (
-                                <div className="mt-2">
-                                    <SecondSpinner />
-                                </div>
-                            )
-                        }
-                    </button>
+                    <form className="relative mb-4 w-full max-w-md ">
+                        <div className="relative mb-4 w-full">
+                            <UserRound size={20} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="Ingresa tu correo electrónico"
+                                autoComplete="username"
+                                className="bg-gray-100 text-gray-800 py-2 pl-12 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                onChange={event => createFormSetter("email")(event.target.value)}
+                                value={formState.email}
+                            />
+                        </div>
+
+                        <div className="relative mb-4 w-full">
+                            <KeyRound size={20} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Ingresa tu contraseña"
+                                autoComplete="current-password"
+                                className="bg-gray-100 text-gray-800 py-2 pl-12 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                onChange={event => createFormSetter("password")(event.target.value)}
+                                value={formState.password}
+                            />
+                        </div>
+                        {error && <p className="text-red-500 mt-1">{error}</p>}
+
+                        <div className='flex justify-center items-center'>
+                            <button
+                                className="bg-light-blue text-white py-2 px-20 rounded-lg hover:bg-blue-600 transition duration-300 relative"
+                                onClick={handleLogin}
+                                disabled={loginMutation.isLoading}
+                            >
+                                {!loginMutation.isLoading ? 'Login' :
+                                    loginMutation.isLoading && (
+                                        <div className="mt-2">
+                                            <SecondSpinner />
+                                        </div>
+                                    )
+                                }
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
             <div className="bg-strong-blue w-1/2 flex items-center justify-center">
