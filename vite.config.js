a3655090a72eba8c129c1a8b649a38895710ae9d
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-08-19 15:42:17 -0600

    adding absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/Components'),
+      '@screens': path.resolve(__dirname, './src/Screens'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@api': path.resolve(__dirname, './src/Api'),
+      '@utils': path.resolve(__dirname, './src/Utils'),
+      '@context': path.resolve(__dirname, './src/Context'),
+    },
+  },
+});
