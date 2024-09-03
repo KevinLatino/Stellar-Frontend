92bfe75f0f98ffbba55a5ee0781863c37578d5ed
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-09-03 13:14:04 -0600

    adding whileHover from framer-motion over some elements

--- src/Components/Calendar/ToolBarComponent.jsx
+++ src/Components/Calendar/ToolBarComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ChevronRight, ChevronLeft } from 'lucide-react';
+import { motion } from 'framer-motion';
 import './CalendarStyles.css';
 
 const ToolbarComponent = (props) => {
@@ -18,23 +19,25 @@ const ToolbarComponent = (props) => {
 
     return (
         <div className="rbc-toolbar flex items-center gap-2">
-            <button
+            <motion.button
+                whileHover={{ scale: 1.2 }}
                 onClick={() => onNavigate('PREV')}
                 style={buttonStyle}
                 onFocus={(e) => e.target.style.outline = 'none'}
                 onMouseDown={(e) => e.preventDefault()}
             >
                 <ChevronLeft size={31} color='#4461F2' />
-            </button>
+            </motion.button>
             <span className='text-stellar-blue text-2xl font-semibold'>{label}</span>
-            <button
+            <motion.button
+                whileHover={{ scale: 1.2 }}
                 onClick={() => onNavigate('NEXT')}
                 style={buttonStyle}
                 onFocus={(e) => e.target.style.outline = 'none'}
                 onMouseDown={(e) => e.preventDefault()}
             >
                 <ChevronRight size={31} color='#4461F2' />
-            </button>
+            </motion.button>
         </div>
     );
 };
